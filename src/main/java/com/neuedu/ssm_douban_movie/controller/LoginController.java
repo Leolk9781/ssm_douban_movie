6ce6ff9ded20a76bfd5387c1f81db35a3f95f054
k9781 <709181562@qq.com>
2019-08-14 09:42:58 +0800

    update -0.0.2

--- src/main/java/com/neuedu/ssm_douban_movie/controller/LoginController.java
+++ src/main/java/com/neuedu/ssm_douban_movie/controller/LoginController.java
@@ -2,29 +2,46 @@ package com.neuedu.ssm_douban_movie.controller;
 
 import com.neuedu.ssm_douban_movie.entity.User;
 import com.neuedu.ssm_douban_movie.service.LoginService;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 
 @Controller
 public class LoginController {
 
+
 	@Autowired
 	private LoginService loginService;
 
-	@RequestMapping("/pages/login")
-	public  String login(HttpServletRequest req, User user){
-		String str = "/index.jsp";
+	@RequestMapping("login")
+	public String login() {
+		return "login";
+	}
 
-		User user1 = loginService.login(user);
-		if (user1==null){
-			str = "/login.jsp";
-			req.setAttribute("msg","用户名或密码不能为空");
+	@RequestMapping("index")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping("login")
+	public  String login(String uId, String pwd){
+
+		User user1 = new User();
+		user1.setuId(uId);
+		user1.setuPasswd(pwd);
+		user1 = loginService.login(user1);
+		HashMap<String, String> map=new HashMap<String, String>();
+		if (user1==null) {
+			map.put("msg", "fail");
+		}else {
+			map.put("msg", "success");
 		}
-
-		return str;
+		JSONObject object=new JSONObject(map);
+		return object.toJSONString();
 	}
 
 }
